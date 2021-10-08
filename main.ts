4f3de545c51df8e14da5a3c32af4790c97387cd1
egudenpa <90732513+egudenpa@users.noreply.github.com>
2021-10-08 20:37:18 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,75 +1,19 @@
-function ゆっくり点灯2 (flg: boolean) {
-    if (flg == true) {
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        strip.setBrightness(80)
-        LEDNo = 0
-    } else if (flg == false) {
-        SIN = 40 * Math.sin(明るさ) + 40
-        if (SIN <= 0.1) {
-            LEDNo = randint(0, 30)
-        }
-        strip.setBrightness(SIN)
-        i = 0
-        for (let index = 0; index < 5; index++) {
-            j = 0
-            for (let index = 0; index < 5; index++) {
-                strip.setPixelColor(i * 29 + j, neopixel.hsl(i * 72 + j, 200, 20))
-                j += 1
-            }
-            i += 1
-        }
-        strip.show()
-        strip.rotate(1)
-        明るさ += 0.2
-        basic.pause(10)
-    }
-}
 function CHECKMODE () {
+    電流 = 0
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    if (MODE == 0) {
+    if (MODE == 1) {
         レインボー点灯(true)
-    } else if (MODE == 1) {
-        ゆっくり点灯(true)
     } else if (MODE == 2) {
-        ゆっくり点灯2(true)
+        白流れ星(true)
     } else if (MODE == 3) {
-        コメット(true)
+        虹色ウェーブ(true)
     } else if (MODE == 4) {
-        ゆっくり点灯3(true)
+        コメット(true)
+    } else if (MODE == 5) {
+        赤ドクドク(true)
     } else {
-        MODE = 0
+        MODE = 1
         CHECKMODE()
-    }
-}
-function ゆっくり点灯 (flg: boolean) {
-    if (flg == true) {
-        strip.setBrightness(80)
-        LEDNo = 0
-    } else if (flg == false) {
-        LEDNo = randint(0, 143)
-        strip.setBrightness(80)
-        i = 0
-        for (let index = 0; index < 20; index++) {
-            strip.setPixelColor(LEDNo, neopixel.colors(NeoPixelColors.White))
-            strip.rotate(1)
-            strip.show()
-            basic.pause(20)
-            i += 1
-        }
-        i = 0
-        for (let index = 0; index < 20; index++) {
-            strip.rotate(1)
-            strip.show()
-            basic.pause(20)
-        }
-        i = 0
-        for (let index = 0; index < 20; index++) {
-            strip.setPixelColor(LEDNo + (i + 20), neopixel.colors(NeoPixelColors.Black))
-            strip.show()
-            basic.pause(20)
-            i += 1
-        }
-        strip.showColor(neopixel.colors(NeoPixelColors.Black))
     }
 }
 input.onButtonPressed(Button.A, function () {
@@ -77,32 +21,45 @@ input.onButtonPressed(Button.A, function () {
 })
 function コメット (flg: boolean) {
     if (flg == true) {
-        strip.setBrightness(80)
+        strip.setBrightness(255)
         i = 0
         for (let index = 0; index < 5; index++) {
             j = 0
-            for (let index = 0; index < 5; index++) {
+            for (let index = 0; index < 7; index++) {
                 strip.setPixelColor(i * 29 + j, neopixel.hsl(i * 72 + j * 10, 200, 25))
                 j += 1
             }
             i += 1
         }
     } else if (flg == false) {
-        strip.rotate(2)
+        strip.rotate(-1)
         strip.show()
+        電流値()
+        basic.pause(10)
     }
 }
-function ゆっくり点灯3 (flg: boolean) {
+function 虹色ウェーブ (flg: boolean) {
     if (flg == true) {
         strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        strip.setBrightness(80)
-        明るさ = 0
+        strip.setBrightness(240)
+        LEDNo = 0
     } else if (flg == false) {
-        明るさ += 0.2
-        SIN = 15 * Math.sin(明るさ) + 15
+        SIN = 120 * Math.sin(明るさ) + 120
         strip.setBrightness(SIN)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(40)
+        i = 0
+        for (let index = 0; index < 15; index++) {
+            j = 0
+            for (let index = 0; index < 1; index++) {
+                strip.setPixelColor(i * 10 + j, neopixel.hsl(i * 24 + j, 200, 20))
+                j += 1
+            }
+            i += 1
+        }
+        strip.rotate(1)
+        strip.show()
+        電流値()
+        明るさ += 0.2
+        basic.pause(10)
     }
 }
 input.onSound(DetectedSound.Loud, function () {
@@ -113,33 +70,87 @@ function レインボー点灯 (flg: boolean) {
     if (flg == true) {
         strip.setBrightness(80)
         strip.showRainbow(1, 360)
+        電流値()
     } else if (flg == false) {
         strip.rotate(2)
         strip.show()
+        電流値()
+    }
+}
+function 白流れ星 (flg: boolean) {
+    if (flg == true) {
+        strip.setBrightness(80)
+        LEDNo = 0
+    } else if (flg == false) {
+        LEDNo = randint(0, 143)
+        strip.setBrightness(100)
+        i = 0
+        for (let index = 0; index < 20; index++) {
+            strip.setPixelColor(LEDNo, neopixel.colors(NeoPixelColors.White))
+            strip.rotate(1)
+            strip.show()
+            電流値()
+            i += 1
+            basic.pause(20)
+        }
+        for (let index = 0; index < 20; index++) {
+            strip.rotate(1)
+            strip.show()
+            電流値()
+            basic.pause(20)
+        }
+        i = 0
+        for (let index = 0; index < 20; index++) {
+            strip.setPixelColor(LEDNo + (i + 20), neopixel.colors(NeoPixelColors.Black))
+            strip.show()
+            電流値()
+            basic.pause(20)
+            i += 1
+        }
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
     }
 }
 function SHOWAURA () {
-    if (MODE == 0) {
+    if (MODE == 1) {
         レインボー点灯(false)
-    } else if (MODE == 1) {
-        ゆっくり点灯(false)
     } else if (MODE == 2) {
-        ゆっくり点灯2(false)
+        白流れ星(false)
     } else if (MODE == 3) {
-        コメット(false)
+        虹色ウェーブ(false)
     } else if (MODE == 4) {
-        ゆっくり点灯3(false)
+        コメット(false)
+    } else if (MODE == 5) {
+        赤ドクドク(false)
     } else {
-        MODE = 0
+        MODE = 1
     }
 }
 input.onButtonPressed(Button.B, function () {
-    MODE = 4
+    MODE = 1
     CHECKMODE()
 })
+function 電流値 () {
+    if (strip.power() >= 電流) {
+        電流 = strip.power()
+    }
+}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showString("" + (strip.power()))
+    basic.showString("" + (電流))
 })
+function 赤ドクドク (flg: boolean) {
+    if (flg == true) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        strip.setBrightness(255)
+        明るさ = 0
+    } else if (flg == false) {
+        明るさ += 0.2
+        SIN = 125 * Math.sin(明るさ) + 125
+        strip.setBrightness(SIN)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        電流値()
+        basic.pause(40)
+    }
+}
 function レインボー切り替え (flg: boolean) {
     if (flg == true) {
         strip.setBrightness(80)
@@ -147,6 +158,7 @@ function レインボー切り替え (flg: boolean) {
     } else if (flg == false) {
         色相 += 10
         strip.showColor(neopixel.hsl(色相, 200, 25))
+        電流値()
         basic.pause(100)
         if (色相 >= 260) {
             色相 = 0
@@ -154,17 +166,19 @@ function レインボー切り替え (flg: boolean) {
     }
 }
 let 色相 = 0
-let j = 0
-let i = 0
 let 明るさ = 0
 let SIN = 0
 let LEDNo = 0
+let j = 0
+let i = 0
+let 電流 = 0
 let MODE = 0
 let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 144, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 150, NeoPixelMode.RGB)
 music.setVolume(30)
 soundExpression.happy.play()
 MODE = 1
+input.setSoundThreshold(SoundThreshold.Loud, 30)
 CHECKMODE()
 basic.forever(function () {
     SHOWAURA()
